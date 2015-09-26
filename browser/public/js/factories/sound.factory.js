083ed6380b759efdc9d7c15fe65651eb2bc5f836
Daniel Moennich <daniel.moennich@gmail.com>
2015-09-26 15:16:08 -0400

    Tried out sound.

--- browser/public/js/factories/sound.factory.js
+++ browser/public/js/factories/sound.factory.js
@@ -4,8 +4,19 @@ app.factory("Sound", function () {
 
 	sound.talk = function (actor, message) {
 		return new Promise(function (resolve, reject) {
-			console.log(actor.name, "says with", actor.gender, "voice:", message);
-			window.setTimeout(resolve,4000);
+
+			// console.log(actor.name, "says with", actor.gender, "voice:", message);
+			// window.setTimeout(resolve,4000);
+
+			var msg = new SpeechSynthesisUtterance(message);
+			msg.onend = function (e) {
+				resolve();
+			};
+			window.speechSynthesis.speak(msg);
+			console.log("MSG", msg);
+
+
+
 		});
 	};
 
