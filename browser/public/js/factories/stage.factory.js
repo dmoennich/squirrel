134e5f9516a1581242a94aa3573e38b6afa83d3e
Daniel Moennich <daniel.moennich@gmail.com>
2015-09-26 17:43:15 -0400

    Added styling for actor label.

--- browser/public/js/factories/stage.factory.js
+++ browser/public/js/factories/stage.factory.js
@@ -4,27 +4,12 @@ app.factory("Stage", function () {
 
 	var stageWidth = window.innerWidth,
 		stageHeight = window.innerHeight,
-		// ctx = canvas.getContext('2d'),
 		actorSize = 70,
 		xmin = Math.floor((stageWidth / 100) * 20),
 		xmax = stageWidth - xmin - actorSize,
 		ymin = Math.floor((stageHeight / 100) * 50) - actorSize,
 		ymax = stageHeight - actorSize,
 		fontSize = 24;
-
-		// canvas.width = stageWidth;
-		// canvas.height = stageHeight;
-		// ctx.font = fontSize + "px serif";
-
-
-	// stage.setStage = function (stageObj) {
-	// 	this.stage = stageObj;
-	// };
-
-	// stage.drawStage = function () {
-	// 	ctx.drawImage(this.stage.floor.img, 0, 0, stageWidth, stageHeight);
-	// 	ctx.drawImage(this.stage.curtain.img, 0, 0, stageWidth, stageHeight);
-	// };
 
 	var getRandomCoord = function () {
 		return {
@@ -33,23 +18,6 @@ app.factory("Stage", function () {
 		};
 	};
 
-
-	// stage.initActors = function (actors) {
-	// 	this.actors = actors;
-
-	// 	var getRandomCoord = function () {
-	// 		return {
-	// 			x: xmin + Math.round(Math.random() * (xmax-xmin)),
-	// 			y: ymin + Math.round(Math.random() * (ymax-ymin))
-	// 		};
-	// 	};
-
-	// 	actors.forEach(function (actor) {
-	// 		actor.coord = getRandomCoord();
-	// 	});
-	// };
-
-
 	stage.placeActors = function (actors) {
 		var stageFloor = angular.element(document.querySelector('#stagefloor'));
 		actors.forEach(function (actor) {
@@ -57,20 +25,14 @@ app.factory("Stage", function () {
 			var element = stageFloor.children().last();
 			var coord = getRandomCoord();
 			element.append("<img src='" +actor.picUrl+ "'>");
-			element.append("<div>" + actor.name + "</div>");
+			element.append("<div class='actor-label'>" + actor.name + "</div>");
 			element.css({
 				left: coord.x + "px",
 				top: coord.y + "px"
 			});
 			actor.element = element;
-			// console.log("ELEMENT:", element);
-			// console.log("ONE ACTOR:", scope.actor);
-
-
 
 		});
-		// ctx.drawImage(actor.img, actor.coord.x, actor.coord.y, actorSize, actorSize);
-		// ctx.fillText(actor.name, actor.coord.x, actor.coord.y + actorSize + fontSize);
 
 	};
 
