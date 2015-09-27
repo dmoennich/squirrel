77f08abe53881828f1f6da1a364a7aace1f9e2a0
Daniel Moennich <daniel.moennich@gmail.com>
2015-09-27 15:11:23 -0400

    Added IMDB quote extraction

--- datasources/imdb.datasource.js
+++ datasources/imdb.datasource.js
@@ -77,17 +77,70 @@ ImdbData.getMovieQuotes = function (movieTitle) {
 	})
 	.then(function ($quotesPage) {
 
+		var conversations = [];
+
+		var quotes = $quotesPage("#quotes_content .sodatext");
+		quotes = Array.prototype.slice.call(quotes, 0);
+		quotes = quotes.map(function (quote) {
+			var conv = $quotesPage(quote).text();
+			conv = conv.replace(/\[[^\]]*\]/g, ""); // removes [....]
+			return conv;
+		});
+
+		quotes.forEach(function (quote) {
+			var conversation = [];
+			var characters = {};
+			var lines = quote.split("\n");
+
+			// remove empty lines
+			lines = lines.filter(function (line) {
+				return line.length > 0;
+			});
+
+			for (var i = 0; i < lines.length - 2; i += 2) {
+				var character = lines[i].substring(0, lines[i].length - 1);
+				var line = lines[i + 1].trim();
+				conversation.push({
+					character: character,
+					message: line
+				});
+				characters[character] = 1;
+			}
+			conversations.push({
+				characters: characters,
+				lines: conversation
+			});
+
+		});
+
+		return conversations;
+
 	});
 };
 
 
-ImdbData.getMovieCharacters("Star Wars").then(function (characters) {
-	console.log("characters length:", characters.length);
-	console.log("characters:", characters[0]);
-	console.log("characters:", characters[1]);
-	console.log("characters:", characters[2]);
-	console.log("characters:", characters[3]);
+ImdbData.getMovieQuotes("Star Wars").then(function (quotes) {
+	console.log("quotes length:", quotes.length);
+
+
+	console.log(quotes[0]);
+
+
+	// console.log("conversation:", conversation);
+	// console.log("characters:", characters);
+	// console.log("quotes:", quotes[1]);
+	// console.log("quotes:", quotes[2]);
+	// console.log("quotes:", quotes[3]);
 });
+
+
+// ImdbData.getMovieCharacters("Star Wars").then(function (characters) {
+// 	console.log("characters length:", characters.length);
+// 	console.log("characters:", characters[0]);
+// 	console.log("characters:", characters[1]);
+// 	console.log("characters:", characters[2]);
+// 	console.log("characters:", characters[3]);
+// });
 
 
 
